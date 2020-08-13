bf8ce083b8a83b30d1be11a88fddef73de517252
Benjamin Bärthlein <benjamin.baerthlein@paessler.com>
2020-08-13 10:35:43 +0200

    add versioning, travis build and deployment

---
+++ cmd/version.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/bbaerthlein/docker-tools/config"
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "show version",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(config.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
